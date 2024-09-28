e75f916159908f177502029303f2864e6cf24dbd
wnas488312 <wiktor.naszydlowski@gmail.com>
2024-09-28 22:31:39 +0200

    Generate Spring boot project

---
+++ src/main/java/com/wnas/images_combiner/ImagesCombinerApplication.java
@@ -0,0 +1,13 @@
+package com.wnas.images_combiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesCombinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesCombinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wnas/images_combiner/ImagesCombinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnas.images_combiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesCombinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
